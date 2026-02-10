78ba1a6fccb39617eb906c67b6a062dcb6484139
DestebanJV <davidji@iadb.org>
2026-02-10 11:41:08 -0500

    Feat: updaet theme base

---
+++ themes/acquia/js/acquia.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * acquia behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.acquia = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
